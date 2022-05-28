84bfbf4c607ab5fc0010787c2a4c25f693a5a24c
jocelynpyw <jocelywotcheu2@gmail.com>
2022-05-28 01:12:35 +0100

    connexion admin

--- frontend/src/screen/inscriptionScreens/AdminConnexionScreen.js
+++ frontend/src/screen/inscriptionScreens/AdminConnexionScreen.js
@@ -1,11 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { toast } from "react-toastify";
-import {
-  loginRectorat,
-  resetRectorat,
-} from "../../redux/authentification/authRectoratSlice";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "../../Styles/AdminConnexionScreen.css";
 import LoadingScreen from "../LoadingScreen";
 import "../../Styles/AdminConnexionScreen.css";
@@ -17,24 +14,24 @@ import {
 function AdminConnexionScreen() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [code2, setCode2] = useState();
+
   const [user, setUser] = useState({
     email: "",
-    code1: "",
-    code2: "",
+    code: "",
   });
 
-  const { message, admin, isError, isLoading, isSuccess } = useSelector(
-    (state) => state.authRectorat
-  );
+  const { message, admin, isError, isLoading, isSuccess, isRejected } =
+    useSelector((state) => state.authAdmin);
   useEffect(() => {
-    if (isError) {
-      alert(message);
+    if (isError || isRejected) {
+      toast.error(message);
     }
-    if (isSuccess) {
+    if (isSuccess || admin) {
       toast.success("Connexion Reussie");
       alert("connexion Reussie");
 
-      navigate("/account");
+      navigate("/connexion/admin");
     }
     if (isLoading) {
       return <LoadingScreen />;
@@ -43,93 +40,107 @@ function AdminConnexionScreen() {
   }, [admin, isSuccess, isError, message, navigate, dispatch]);
 
   const SubmitHandle = (e) => {
-    if (user.code1 === user.code2) {
-      dispatch(loginAdmin(user));
-    } else {
-      alert("les codes ne correspondent pas ");
-      console.log(
-        `les code 1 est ${user.code1} et le code 2 est ${user.code2} et l'email de cet utilisateur est ${user.email}`
-      );
+    if (code2 === "" || user.code === "" || user.email === "") {
+      toast.warning("Remplissez tout vos champs");
       e.preventDefault();
+    } else {
+      if (user.code === code2) {
+        dispatch(loginAdmin(user));
+      } else {
+        toast.warning("les codes ne correspondent pas");
+
+        console.log(
+          `les code 1 est ${user.code1} et le code 2 est ${user.code2} et l'email de cet utilisateur est ${user.email}`
+        );
+        e.preventDefault();
+      }
     }
+
     e.preventDefault();
   };
   return (
-    <div style={{ padding: "4%" }} className="container-connexion">
-      <div className="container">
-        <h1 className="inscription-etudiant-title">Connexion Administrateur</h1>
+    <div>
+      {isLoading === true ? (
+        <LoadingScreen />
+      ) : (
+        <div style={{ padding: "4%" }} className="container-connexion">
+          <div className="container">
+            <h1 className="inscription-etudiant-title">
+              Connexion Administrateur
+            </h1>
+            <ToastContainer />
 
-        <div className="row">
-          <div className="col-md-2"></div>
-          <div className="col-md-8 container-data-connexion">
             <div className="row">
-              <div className="col-md-6 container-data-connexion-left">
-                <div className="cover-left-image"></div>
-              </div>
-              <div className="col-md-6 container-data-connexion-right">
-                <form className="row g-3">
-                  <div className="col-12">
-                    <label htmlFor="email" className="form-label">
-                      Adresse Email
-                    </label>
-                    <input
-                      type="email"
-                      className="form-control"
-                      id="email"
-                      onChange={(e) =>
-                        setUser({ ...user, email: e.target.value })
-                      }
-                    />
+              <div className="col-md-2"></div>
+              <div className="col-md-8 container-data-connexion">
+                <div className="row">
+                  <div className="col-md-6 container-data-connexion-left">
+                    <div className="cover-left-image"></div>
                   </div>
-                  <div className="col-12">
-                    <label htmlFor="codeSecret1" className="form-label">
-                      Code secret 1
-                    </label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="codeSecret1"
-                      onChange={(e) =>
-                        setUser({ ...user, code1: e.target.value })
-                      }
-                    />
-                  </div>
-                  <div className="col-12">
-                    <label htmlFor="codeSecret2" className="form-label">
-                      Code secret 2
-                    </label>
-                    <input
-                      type="text"
-                      className="form-control"
-                      id="codeSecret2"
-                      onChange={(e) =>
-                        setUser({ ...user, code2: e.target.value })
-                      }
-                    />
-                  </div>
-                  <br />
-                  <div className="d-grid gap-2">
-                    <button
-                      className="btn btn-primary btn-connexion"
-                      type="submit"
-                      style={{ marginTop: "5px" }}
-                      onClick={SubmitHandle}
-                    >
-                      Se Connecter
-                    </button>
-                  </div>
-                  {/* <div className="col-12">
+                  <div className="col-md-6 container-data-connexion-right">
+                    <form className="row g-3">
+                      <div className="col-12">
+                        <label htmlFor="email" className="form-label">
+                          Adresse Email
+                        </label>
+                        <input
+                          type="email"
+                          className="form-control"
+                          id="email"
+                          onChange={(e) =>
+                            setUser({ ...user, email: e.target.value })
+                          }
+                        />
+                      </div>
+                      <div className="col-12">
+                        <label htmlFor="codeSecret1" className="form-label">
+                          Code secret 1
+                        </label>
+                        <input
+                          type="password"
+                          className="form-control"
+                          id="codeSecret1"
+                          onChange={(e) =>
+                            setUser({ ...user, code: e.target.value })
+                          }
+                        />
+                      </div>
+                      <div className="col-12">
+                        <label htmlFor="codeSecret2" className="form-label">
+                          Code secret 2
+                        </label>
+                        <input
+                          type="password"
+                          className="form-control"
+                          id="codeSecret2"
+                          onChange={(e) => setCode2(e.target.value)}
+                        />
+                      </div>
+                      <br />
+                      <div className="d-grid gap-2">
+                        <button
+                          className="btn btn-primary btn-connexion"
+                          type="submit"
+                          style={{ marginTop: "5px" }}
+                          onClick={SubmitHandle}
+                        >
+                          Se Connecter
+                        </button>
+                      </div>
+                      {/* <div className="col-12">
                   <button type="submit" className="btn btn-primary">
                     Sign in
                   </button>
                 </div> */}
-                </form>
+                    </form>
+                  </div>
+                </div>
               </div>
+              <div className="col-md-2"></div>
             </div>
           </div>
-          <div className="col-md-2"></div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
