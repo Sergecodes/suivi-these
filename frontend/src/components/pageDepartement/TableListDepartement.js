d09d927e2a4e381804497353a1248fea065f6da5
kuntz stephane nchouwet <kuntzstephanenchouwetmfouapon@gmail.com>
2022-06-27 11:00:29 +0100

    ajout option modification jury etudiant à departement

--- frontend/src/components/pageDepartement/TableListDepartement.js
+++ frontend/src/components/pageDepartement/TableListDepartement.js
@@ -1,355 +1,244 @@
-import { Table } from "antd";
-import React from "react";
+import { Table, Modal, Select, Button } from "antd";
+import React, { useState } from "react";
 import moment from "moment";
-import {MdSend} from "react-icons/md"
-
-import {ImCross, ImCheckmark} from "react-icons/im";
-import { Link } from "react-router-dom";
-
-
-const columns = [
-  {
-    title: "Photo ",
-    dataIndex: "photo",
-  },
-  {
-    title: "Matricule",
-    dataIndex: "matricule",
-    sorter: {
-      compare: (a, b) => a.matricule.localeCompare(b.matricule),
-    },
-  },
-  {
-    title: "Nom et Prenom",
-    dataIndex: "name",
-    sorter: {
-      compare: (a, b) => a.name.localeCompare(b.name),
-    },
-  },
-  {
-    title: "Date Envoi",
-    dataIndex: "dateEnvoi",
-    sorter: {
-      compare: (a, b) =>
-        moment(a.dateEnvoi).unix() - moment(b.dateEnvoi).unix(),
-    },
-  },
-  {
-    title: "Date Notation",
-    dataIndex: "dateNotation",
-    sorter: {
-      compare: (a, b) =>
-        moment(a.dateEnvoi).unix() - moment(b.dateEnvoi).unix(),
-    },
-  },
-  {
-    title: "Statut",
-    dataIndex: "statut",
-  },
-];
-var today = new Date();
-
-const data = [
-  {
-    key: "1",
-    photo: (
-      <img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1nf1W7VULCSp751rP0AxpCPvOzoN9XKDO0Q&usqp=CAU"
-        alt="cc"
-        className="rounded-circle"
-        style={{ width: "50px", height: "50px" }}
-      />
-    ),
-    matricule: <div>19M2214</div>,
-    name: <div className="fs-6 fw-light">Nchouwet Mfouapon Kuntz Stephane</div>,
-    dateEnvoi: today.toLocaleString("en-US"),
-    dateNotation: '---',
-    statut: (
-      <div>
-        <Link to="/acteur/departement/verification">
-          <button
-            type="button"
-            className="btn py-1"
-            style={{
-              color: "white",
-              cursor: "pointer",
-              backgroundColor: "var(--secondaryColor)",
-            }}
-          >
-            <MdSend/> Consulter
-          </button>
-        </Link>
-      </div>
-    ),
-  },
-  {
-    key: "78",
-    photo: (
-      <img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1nf1W7VULCSp751rP0AxpCPvOzoN9XKDO0Q&usqp=CAU"
-        alt="cc"
-        className="rounded-circle"
-        style={{ width: "50px", height: "50px" }}
-      />
-    ),
-    matricule: <div>19M2214</div>,
-    name: <div className="fs-6 fw-light">Nchouwet Mfouapon Kuntz Stephane</div>,
-    dateEnvoi: today.toLocaleString("en-US"),
-    dateNotation: today.toLocaleString("en-US"),
-    statut:<Link to="/acteur/departement/verification">
-    <button
-      type="button"
-      className="btn py-1"
-      style={{
-        color: "green",
-        cursor: "pointer",
-        backgroundColor: "transparent",
-      }}
-    >
-      <ImCheckmark /> Validé
-    </button>
-  </Link>,
-  },
-  {
-    key: "2",
-    photo: (
-      <img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1nf1W7VULCSp751rP0AxpCPvOzoN9XKDO0Q&usqp=CAU"
-        alt="cc"
-        className="rounded-circle"
-        style={{ width: "50px", height: "50px" }}
-      />
-    ),
-    matricule: <div>19M2214</div>,
-    name: <div className="fs-6 fw-light">Nchouwet Mfouapon Kuntz Stephane</div>,
-    dateEnvoi: today.toLocaleString("en-US"),
-    dateNotation: '---',
-    statut:<div>
-    <Link to="/acteur/departement/verification">
-      <button
-        type="button"
-        className="btn py-1"
-        style={{
-          color: "white",
-          cursor: "pointer",
-          backgroundColor: "var(--secondaryColor)",
-        }}
-      >
-        <MdSend/> Consulter
-      </button>
-    </Link>
-  </div>,
-  },
-  {
-    key: "3",
-    photo: (
-      <img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1nf1W7VULCSp751rP0AxpCPvOzoN9XKDO0Q&usqp=CAU"
-        alt="cc"
-        className="rounded-circle"
-        style={{ width: "50px", height: "50px" }}
-      />
-    ),
-    matricule: <div>19M2214</div>,
-    name: <div className="fs-6 fw-light">Nchouwet Mfouapon Kuntz Stephane</div>,
-    dateEnvoi: today.toLocaleString("en-US"),
-    dateNotation: '---',
-    statut: <div>
-    <Link to="/acteur/departement/verification">
-      <button
-        type="button"
-        className="btn py-1"
-        style={{
-          color: "white",
-          cursor: "pointer",
-          backgroundColor: "var(--secondaryColor)",
-        }}
-      >
-        <MdSend/> Consulter
-      </button>
-    </Link>
-  </div>,
-  },
-  {
-    key: "4",
-    photo: (
-      <img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1nf1W7VULCSp751rP0AxpCPvOzoN9XKDO0Q&usqp=CAU"
-        alt="cc"
-        className="rounded-circle"
-        style={{ width: "50px", height: "50px" }}
-      />
-    ),
-    matricule: <div>19M2214</div>,
-    name: <div className="fs-6 fw-light">Nchouwet Mfouapon Kuntz Stephane</div>,
-    dateEnvoi: today.toLocaleString("en-US"),
-    dateNotation: today.toLocaleString("en-US"),
-    statut: <Link to="/acteur/departement/verification">
-    <button
-      type="button"
-      className="btn py-1"
-      style={{
-        color: "red",
-        cursor: "pointer",
-        backgroundColor: "transparent",
-      }}
-    >
-      <ImCross /> Rejeté
-    </button>
-  </Link>,
-  },
-  {
-    key: "5",
-    photo: (
-      <img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1nf1W7VULCSp751rP0AxpCPvOzoN9XKDO0Q&usqp=CAU"
-        alt="cc"
-        className="rounded-circle"
-        style={{ width: "50px", height: "50px" }}
-      />
-    ),
-    matricule: <div>19M2214</div>,
-    name: <div className="fs-6 fw-light">Nchouwet Mfouapon Kuntz Stephane</div>,
-    dateEnvoi: today.toLocaleString("en-US"),
-    dateNotation: today.toLocaleString("en-US"),
-    statut: <Link to="/acteur/departement/verification">
-    <button
-      type="button"
-      className="btn py-1"
-      style={{
-        color: "red",
-        cursor: "pointer",
-        backgroundColor: "transparent",
-      }}
-    >
-      <ImCross /> Rejeté
-    </button>
-  </Link>,
-  },
-  {
-    key: "6",
-    photo: (
-      <img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1nf1W7VULCSp751rP0AxpCPvOzoN9XKDO0Q&usqp=CAU"
-        alt="cc"
-        className="rounded-circle"
-        style={{ width: "50px", height: "50px" }}
-      />
-    ),
-    matricule: <div>19M2214</div>,
-    name: <div className="fs-6 fw-light">Nchouwet Mfouapon Kuntz Stephane</div>,
-    dateEnvoi: today.toLocaleString("en-US"),
-    dateNotation: '---',
-    statut: <div>
-    <Link to="/acteur/departement/verification">
-      <button
-        type="button"
-        className="btn py-1"
-        style={{
-          color: "white",
-          cursor: "pointer",
-          backgroundColor: "var(--secondaryColor)",
-        }}
-      >
-        <MdSend/> Consulter
-      </button>
-    </Link>
-  </div>,
-  },
-  {
-    key: "7",
-    photo: (
-      <img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1nf1W7VULCSp751rP0AxpCPvOzoN9XKDO0Q&usqp=CAU"
-        alt="cc"
-        className="rounded-circle"
-        style={{ width: "50px", height: "50px" }}
-      />
-    ),
-    matricule: <div>19M2214</div>,
-    name: <div className="fs-6 fw-light">Nchouwet Mfouapon Kuntz Stephane</div>,
-    dateEnvoi: today.toLocaleString("en-US"),
-    dateNotation: today.toLocaleString("en-US"),
-    statut: <Link to="/acteur/departement/verification">
-    <button
-      type="button"
-      className="btn py-1"
-      style={{
-        color: "green",
-        cursor: "pointer",
-        backgroundColor: "transparent",
-      }}
-    >
-      <ImCheckmark /> Validé
-    </button>
-  </Link>,
-  },
-  {
-    key: "8",
-    photo: (
-      <img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1nf1W7VULCSp751rP0AxpCPvOzoN9XKDO0Q&usqp=CAU"
-        alt="cc"
-        className="rounded-circle"
-        style={{ width: "50px", height: "50px" }}
-      />
-    ),
-    matricule: <div>19M2214</div>,
-    name: <div className="fs-6 fw-light">Nchouwet Mfouapon Kuntz Stephane</div>,
-    dateEnvoi: today.toLocaleString("en-US"),
-    dateNotation: today.toLocaleString("en-US"),
-    statut: <Link to="/acteur/departement/verification">
-    <button
-      type="button"
-      className="btn py-1"
-      style={{
-        color: "green",
-        cursor: "pointer",
-        backgroundColor: "transparent",
-      }}
-    >
-      <ImCheckmark /> Validé
-    </button>
-  </Link>,
-  },
-  {
-    key: "9",
-    photo: (
-      <img
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1nf1W7VULCSp751rP0AxpCPvOzoN9XKDO0Q&usqp=CAU"
-        alt="cc"
-        className="rounded-circle"
-        style={{ width: "50px", height: "50px" }}
-      />
-    ),
-    matricule: <div>19M2214</div>,
-    name: <div className="fs-6 fw-light">Nchouwet Mfouapon Kuntz Stephane</div>,
-    dateEnvoi: today.toLocaleString("en-US"),
-    dateNotation: today.toLocaleString("en-US"),
-    statut: <Link to="/acteur/departement/verification">
-    <button
-      type="button"
-      className="btn py-1"
-      style={{
-        color: "red",
-        cursor: "pointer",
-        backgroundColor: "transparent",
-      }}
-    >
-      <ImCross /> Rejeté
-    </button>
-  </Link>,
-  },
-];
-
-/*function onChange(pagination, filters, sorter, extra) {
-  console.log('params', pagination, filters, sorter, extra);
-}*/
+import { BsPerson, BsPenFill, BsX, BsCheck } from "react-icons/bs";
+import { useNavigate } from "react-router-dom";
 
 const TableListDepartement = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [listeJury, setListeJury] = useState([]);
+  const { Option } = Select;
+  const navigate = useNavigate();
+
+  const columns = [
+    {
+      title: "Photo ",
+      dataIndex: "photo",
+      align: "center",
+    },
+    {
+      title: "Matricule",
+      dataIndex: "matricule",
+      sorter: {
+        compare: (a, b) => a.matricule.localeCompare(b.matricule),
+      },
+      align: "center",
+    },
+    {
+      title: "Nom et Prenom",
+      dataIndex: "name",
+      sorter: {
+        compare: (a, b) => a.name.localeCompare(b.name),
+      },
+      align: "center",
+    },
+    {
+      title: "Date Envoi",
+      dataIndex: "dateEnvoi",
+      sorter: {
+        compare: (a, b) =>
+          moment(a.dateEnvoi).unix() - moment(b.dateEnvoi).unix(),
+      },
+      align: "center",
+    },
+    {
+      title: "Date Notation",
+      dataIndex: "dateNotation",
+      sorter: {
+        compare: (a, b) =>
+          moment(a.dateEnvoi).unix() - moment(b.dateEnvoi).unix(),
+      },
+      align: "center",
+    },
+    {
+      title: "Actions",
+      render: (record) => {
+        return (
+          <div className="d-flex fs-4 justify-content-around ">
+            <BsPerson
+              className="me-2"
+              style={{ color: "#513e8f" }}
+              onClick={() => {
+                showModal();
+                setListeJury(record.jury);
+              }}
+            />
+            <BsPenFill
+              style={{ color: "#513e8f" }}
+              onClick={() => {
+                navigate("/acteur/departement/verification");
+              }}
+            />
+          </div>
+        );
+      },
+      align: "center",
+    },
+  ];
+  var today = new Date();
+
+  const [data, setData] = useState([
+    {
+      key: "1",
+      photo: (
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1nf1W7VULCSp751rP0AxpCPvOzoN9XKDO0Q&usqp=CAU"
+          alt="cc"
+          className="rounded-circle"
+          style={{ width: "50px", height: "50px" }}
+        />
+      ),
+      matricule: "19M1234",
+      name: "nom 1 prenom 1",
+      dateEnvoi: today.toLocaleString("en-US"),
+      dateNotation: "---",
+      jury: function () {
+        let output = [];
+        for (let i = 1; i <= 3; i++) {
+          output.push({
+            id: i,
+            nom: "nom" + i,
+            prenom: "prenom" + i,
+            email: "jury" + i + "@gmail.com",
+          });
+        }
+        return output;
+      },
+    },
+  ]);
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+    setModified(false);
+  };
+
+  
+  const [current, setCurrent] = useState(1);
+  const [index, setIndex] = useState(1);
+
+  const [modified, setModified] = useState(false);
+  const [juryData, setJuryData] = useState(function () {
+    let output = [];
+    for (let i = 1; i < 6; i++) {
+      output.push({
+        id: i,
+        nom: "nom" + i,
+        prenom: "prenom" + i,
+        email: "jury" + i + "@gmail.com",
+      });
+    }
+    return output;
+  });
+  const [selectedJury, setSelectedJury] = useState(juryData);
+  const [tempJury, setTempJury] = useState([]);
+  const handleChange = (value, option) => {
+    let newListe = [...listeJury];
+    newListe[index] = juryData.filter((elt) => elt.email === value);
+    newListe[index] = newListe[index][0];
+    setTempJury(newListe);
+  };
+
   return (
-    <div className=" mx-3 my-3" style={{overflow:"scroll"}}>
+    <div className=" mx-3 my-3" style={{ overflow: "scroll" }}>
       <Table columns={columns} dataSource={data} pagination={{ pageSize: 5 }} />
+      <Modal
+        title="ListeJury"
+        visible={isModalVisible}
+        onOk={handleOk}
+        footer={[
+          <Button key="submit" type="primary" onClick={handleOk}>
+            OK
+          </Button>,
+        ]}
+      >
+        <div>
+          {listeJury.map((jury, index) => {
+            return (
+              <div key={jury.id}>
+                {!(modified && current === jury.id) ? (
+                  <div className="d-flex align-items-center justify-content-around">
+                    <p className="fw-lighter fs-6">{jury.email}</p>
+                    <p
+                      style={
+                        modified === true
+                          ? { display: "none" }
+                          : { color: "#513e8f", cursor: "pointer" }
+                      }
+                      onClick={() => {
+                        setModified(!modified);
+                        setIndex(index);
+                        setCurrent(jury.id);
+                        setSelectedJury(function () {
+                          let output = [],
+                            first = 1,
+                            second = 2;
+                          if (index === 1) {
+                            first = 0;
+                            second = 2;
+                            console.log("enter");
+                          }
+                          if (index === 2) {
+                            first = 0;
+                            second = 1;
+                          }
+                          for (let i in juryData) {
+                            if (
+                              juryData[i].email !== listeJury[first].email &&
+                              juryData[i].email !== listeJury[second].email
+                            ) {
+                              output.push(juryData[i]);
+                            }
+                          }
+
+                          return output;
+                        });
+                      }}
+                    >
+                      Modifier
+                    </p>
+                  </div>
+                ) : (
+                  <div className="d-flex align-items-center justify-content-around my-1">
+                    {
+                      <Select
+                        defaultValue={selectedJury[0].email}
+                        onChange={(value, option) =>
+                          handleChange(value, option)
+                        }
+                      >
+                        {selectedJury.map((elt) => {
+                          return (
+                            <Option key={elt.id} value={elt.email}>
+                              {elt.email}
+                            </Option>
+                          );
+                        })}
+                      </Select>
+                    }
+                    <div className="fs-3">
+                      <BsX
+                        style={{ color: "red", cursor: "pointer" }}
+                        onClick={() => setModified(false)}
+                      />
+                      <BsCheck
+                        className="ms-2"
+                        style={{ color: "green", cursor: "pointer" }}
+                        onClick={() => {
+                          setListeJury(tempJury);
+                          setModified(false);
+                        }}
+                      />
+                    </div>
+                  </div>
+                )}
+              </div>
+            );
+          })}
+        </div>
+      </Modal>
     </div>
   );
 };
